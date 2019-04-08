dccf503c5aef8ed0e368c48add6cbfca3b5b6d04
Bismarck <bismarck.berrios@gmail.com>
2019-04-07 23:24:11 -0600

    TextView con la fecha Actual en Registro_Credito

--- app/src/main/java/com/bisanbl/prestamo/MainActivity.java
+++ app/src/main/java/com/bisanbl/prestamo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.bisanbl.prestamo;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -71,11 +72,24 @@ public class MainActivity extends AppCompatActivity {
         BTNcontinuar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Registro_Credito.class);
-                startActivity(intent);
+
+                if (!validarActivity(ETnombre, ETdireccion, ETTelefono, ETcedula)) {
+                    Snackbar.make(v, getResources().getString(R.string.ErrorBTN),Snackbar.LENGTH_LONG).show();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), Registro_Credito.class);
+                    startActivity(intent);
+                }
+
             }
         });
     }
 
-
+    private boolean validarActivity(EditText ETnombre, EditText ETdireccion, EditText ETTelefono, EditText ETcedula){
+        if (TextUtils.isEmpty(ETnombre.getText().toString()) || TextUtils.isEmpty(ETdireccion.getText().toString())){
+            if (TextUtils.isEmpty(ETTelefono.getText().toString()) || TextUtils.isEmpty(ETcedula.getText().toString())) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
--- app/src/main/java/com/bisanbl/prestamo/Registro_Credito.java
+++ app/src/main/java/com/bisanbl/prestamo/Registro_Credito.java
@@ -2,6 +2,11 @@ package com.bisanbl.prestamo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class Registro_Credito extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class Registro_Credito extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro__credito);
+
+        TextView TVfechainicial = findViewById(R.id.TVFechaInicialM);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy", Locale.getDefault());
+        Date date = new Date();
+
+        TVfechainicial.setText(dateFormat.format(date));
+
     }
 }
