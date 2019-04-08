1b436ea09ab4038e8cc8cce8ec348cf518feb0ed
Bismarck <bismarck.berrios@gmail.com>
2019-04-07 19:31:00 -0600

    Correcciones de MainActivity y agregado el segundo activiti registro_Credito

---
+++ app/src/main/java/com/bisanbl/prestamo/Registro_Credito.java
@@ -0,0 +1,13 @@
+package com.bisanbl.prestamo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Registro_Credito extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro__credito);
+    }
+}
